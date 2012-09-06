1869083831e3c67f56272c966a6d201d36da0c1b
lxy <lxy@hesong.net>
2012-09-06 15:00:33 +0800

    增加Python3.x的支持

--- src/hanzi2pinyin.py
+++ src/hanzi2pinyin.py
@@ -8,8 +8,16 @@ Created on 2012-8-26
 '''
 
 import os
+import types
 import json
 
+# Python3 compatibility:  On python2.5, introduce the bytes alias from 2.6
+try:
+    bytes
+except Exception:
+    bytes = str
+
+# Load HanZi -> PinYin map file to a dict
 _map = json.loads(open(os.path.join(os.path.dirname(__file__), ('%s.json') % (os.path.splitext(os.path.basename(__file__))[0]))).read())
 
 STYLE_CAMEL = 1
@@ -17,12 +25,11 @@ STYLE_LOWER = 2
 STYLE_UPPER = 3
 
 def translate(hanzi, style=STYLE_LOWER, delimiter=' ', encoding='utf-8', firstletter_only=False):
-    if isinstance(hanzi, bytes):
-        hanzi = hanzi.decode(encoding)
-    elif isinstance(hanzi, unicode):
-        hanzi = hanzi
+    if type(hanzi) in types.StringTypes:
+        if isinstance(hanzi, bytes):
+            hanzi = hanzi.decode(encoding)
     else:
-        raise TypeError('parameter hanzi is not str or unicode')
+        raise TypeError('parameter hanzi is not str or bytes')
     result = ''
     for hz_char in hanzi:
         if result:
--- src/test.py
+++ src/test.py
@@ -8,7 +8,6 @@ Created on 2012-8-26
 
 import hanzi2pinyin
 
-
 if __name__ == '__main__':
     print hanzi2pinyin.translate('要转换的汉字', style=hanzi2pinyin.STYLE_CAMEL, delimiter='', firstletter_only=True)
-    print hanzi2pinyin.translate(u'汉字')
+    print hanzi2pinyin.translate(u'信用')
